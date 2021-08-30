8e67f6ce1b600c7e3d489276662a332b70b71b81
Ilaria Refaat <ilariarefaa20@gmail.com>
2021-08-30 15:00:51 +0200

    id not found

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -19,7 +19,7 @@ const blog_details = (req, res) => {
       res.render("blogs/details", { blog: result, title: "Blog Details" });
     })
     .catch((err) => {
-      console.log(err);
+      res.status(404).render("404", { title: "404" });
     });
 };
 const blog_create_get = (req, res) => {
